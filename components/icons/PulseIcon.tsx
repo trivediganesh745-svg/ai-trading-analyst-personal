83aa7416edbdae727ff2228544480015516bb120
Ganesh Trivedi/Corporate Accountant-Controller <trivediganesh745@gmail.com>
2025-09-18 16:44:40 +0530

    Create PulseIcon.tsx

---
+++ components/icons/PulseIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const PulseIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-cyan-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5v-4m0 0h-4m4 0l-5-5" />
+  </svg>
+);
